5a1b90b197e35f9cb608a9c8d878d8632b9f400e
Lucas Wee <lucaswee2000@gmail.com>
2023-11-08 16:05:35 +0800

    fjlafjwneabvnaw

--- src/components/about/AboutMe.vue
+++ src/components/about/AboutMe.vue
@@ -6,7 +6,6 @@
             <div class="flex items-center">
                 <font-awesome-icon :icon="['fab', 'wpexplorer']" size="2x"
 				bounce style="color: #000000;" class="mr-2" />
-				<!-- Font Awesome Icon -->
                 <h1 class="text-4xl font-bold">Explore the dialects!</h1>
             </div>
             
@@ -15,16 +14,26 @@
                 <!-- Filter Funnel Icon -->
                 <img src="https://cdn2.iconfinder.com/data/icons/font-awesome/1792/filter-512.png" 
 				alt="Filter" class="mr-2 h-8 w-8" />
-                <!-- Filter Dropdown -->
-                <select class="border rounded-lg p-2">
-                    <option value="popularity">Popularity</option>
+
+                <!-- First Filter Dropdown -->
+                <select class="border rounded-lg p-2 mr-4 bg-white shadow-sm hover:shadow-md transition-shadow duration-200 ease-in-out">
+                    <option value="most-viewed">Most Viewed</option>
+                    <option value="highest-rating">Highest Rating</option>
                     <option value="newest">Newest to Oldest</option>
                     <option value="oldest">Oldest to Newest</option>
+                </select>
+
+                <!-- Second Filter Dropdown for Categories -->
+                <select class="border rounded-lg p-2 mr-4 bg-white shadow-sm hover:shadow-md transition-shadow duration-200 ease-in-out">
+                    <option value="movies">Movies</option>
+                    <option value="blogs">Blogs</option>
+                    <option value="songs">Songs</option>
                 </select>
             </div>
         </div>
     </div>
 </template>
+
 
 <script>
 export default {
